285c45dd8ef01e14e0d2b82595932daaaa7ed114
bruno.xavier <bruno.xavier@multivis.com>
2025-06-26 14:19:17 -0300

    fix: utils

---
+++ dist/api/utils.js
@@ -0,0 +1,15 @@
+import { clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+import dotenv from "dotenv";
+dotenv.config();
+export function cn(...inputs) {
+    return twMerge(clsx(inputs));
+}
+export default function getOpenAIRandomApiKey() {
+    const keys = process.env.VITE_OPENAI_API_KEYS?.split(",").filter(Boolean);
+    if (!keys || keys.length === 0) {
+        throw new Error("Nenhuma chave da OpenAI encontrada em OPENAI_API_KEYS");
+    }
+    const index = Math.floor(Math.random() * keys.length);
+    return keys[index];
+}
